b71942e80d7934063db6238988e10ee3368562c3
EC2 Default User <ec2-user@ip-172-31-38-122.eu-west-1.compute.internal>
2022-08-10 10:53:04 +0000

    adding some changes

--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -1,5 +1,7 @@
 import * as ecs from '@aws-cdk/aws-ecs';
 import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
+import ec2 = require("@aws-cdk/aws-ec2");
+import cdk = require('@aws-cdk/core');
 
 import { CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
 import * as path from 'path';
@@ -15,14 +17,33 @@ export class CdkpipelinesDemoStack extends Stack {
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+    
+    const vpc = new ec2.Vpc(this, 'ecs-cdk-vpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      maxAzs: 3
+    });
+    
+    const cluster = new ecs.Cluster(this, "ecs-cluster", {
+      vpc: vpc,
+    });
+
 
     // Instantiate Fargate Service with just cluster and image
     const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
       memoryLimitMiB: 1024,
+      cluster: cluster,
       cpu: 512,
       taskImageOptions: {
         image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
       },
+    });
+    
+    const scaling = service.service.autoScaleTaskCount({ maxCapacity: 6 });
+    scaling.scaleOnCpuUtilization('CpuScaling', {
+      targetUtilizationPercent: 10,
+      scaleInCooldown: cdk.Duration.seconds(60),
+      scaleOutCooldown: cdk.Duration.seconds(60)
     });
 
     this.urlOutput = new CfnOutput(this, 'Url', {
