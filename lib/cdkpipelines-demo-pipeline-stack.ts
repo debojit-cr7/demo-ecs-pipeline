6856dfd461535aca6ac11aa8a6fc99224cdb50fd
EC2 Default User <ec2-user@ip-172-31-38-122.eu-west-1.compute.internal>
2022-08-10 09:30:12 +0000

    1st commit

---
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -0,0 +1,62 @@
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
+import { CdkPipeline, ShellScriptAction, SimpleSynthAction } from "@aws-cdk/pipelines";
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
+
+/**
+ * The stack that defines the application pipeline
+ */
+export class CdkpipelinesDemoPipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const sourceArtifact = new codepipeline.Artifact();
+    const cloudAssemblyArtifact = new codepipeline.Artifact();
+
+    const pipeline = new CdkPipeline(this, 'Pipeline', {
+      // The pipeline name
+      pipelineName: 'MyServicePipeline',
+      cloudAssemblyArtifact,
+
+      // Where the source can be found
+      sourceAction: new codepipeline_actions.GitHubSourceAction({
+        actionName: 'GitHub',
+        branch: 'main',
+        output: sourceArtifact,
+        oauthToken: SecretValue.secretsManager('github-token'),
+        owner: 'debojit-cr7',
+        repo: 'demo-ecs-pipeline',
+      }),
+
+       // How it will be built and synthesized
+       synthAction: SimpleSynthAction.standardNpmSynth({
+         sourceArtifact,
+         cloudAssemblyArtifact,
+
+         // We need a build step to compile the TypeScript Fargate Service
+         buildCommand: 'npm run build'
+       }),
+    });
+
+    // This is where we add the application stages
+    const preprod = new CdkpipelinesDemoStage(this, 'PreProd', {
+      env: { account: '361509912577', region: 'eu-west-1' }
+    });
+    const preprodStage = pipeline.addApplicationStage(preprod);
+
+    preprodStage.addActions(new ShellScriptAction({
+      actionName: 'TestService',
+      useOutputs: {
+        ENDPOINT_URL: pipeline.stackOutput(preprod.urlOutput),
+      },
+      commands: [
+        'curl -Ssf $ENDPOINT_URL',
+      ],
+    }));
+
+    pipeline.addApplicationStage(new CdkpipelinesDemoStage(this, 'Staging', {
+      env: { account: '361509912577', region: 'eu-west-1' }
+    }));
+  }
+}
--- lib/cdkpipelines-demo-stack.ts
+++ lib/cdkpipelines-demo-stack.ts
@@ -1,16 +1,32 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as ecs from '@aws-cdk/aws-ecs';
+import ecs_patterns = require('@aws-cdk/aws-ecs-patterns');
 
+import { CfnOutput, Construct, Stack, StackProps } from '@aws-cdk/core';
+import * as path from 'path';
+
+/**
+ * A stack for our simple Application Load Balanced Fargate Service
+ */
 export class CdkpipelinesDemoStack extends Stack {
+  /**
+   * The DNS endpoint of the LoadBalancer
+   */
+  public readonly urlOutput: CfnOutput;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Instantiate Fargate Service with just cluster and image
+    const service = new ecs_patterns.ApplicationLoadBalancedFargateService(this, "FargateService", {
+      memoryLimitMiB: 1024,
+      cpu: 512,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('amazon/amazon-ecs-sample'),
+      },
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkpipelinesDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    this.urlOutput = new CfnOutput(this, 'Url', {
+      value: service.loadBalancer.loadBalancerDnsName,
+    });
   }
 }
